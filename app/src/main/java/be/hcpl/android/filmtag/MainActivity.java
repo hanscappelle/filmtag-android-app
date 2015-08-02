a1200edbd026e6fbc2c56108db68ced45d944854
Hans <hans@hcpl.be>
2015-08-02 11:45:08 +0200

    proper fragment resume

--- app/src/main/java/be/hcpl/android/filmtag/MainActivity.java
+++ app/src/main/java/be/hcpl/android/filmtag/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,7 +13,16 @@ import android.support.v7.widget.Toolbar;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_CURRENT_CONTENT = "current_content";
+    /**
+     * app preferences, only kept here
+     */
     private SharedPreferences prefs;
+
+    /**
+     * the currently visible fragment
+     */
+    private Fragment mContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +34,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
         setSupportActionBar(toolbar);
 
-        // always starts with the same initial fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, FilmRollListFragment.newInstance()).commit();
-
+        // load the prefs here
         prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        FragmentManager fm = getSupportFragmentManager();
+        // always starts with the same initial fragment
+//        fm.beginTransaction().replace(R.id.container, FilmRollListFragment.newInstance()).commit();
+        // restore last fragment state if possible
+        if (savedInstanceState != null) {
+            mContent = fm.getFragment(savedInstanceState, KEY_CURRENT_CONTENT);
+        }
+
+        if (mContent == null && fm.getFragments() == null || fm.getFragments().isEmpty() || fm.getFragments().size() == 1) {
+            mContent = getInitialFragment();
+        }
+
+        if (mContent != null && !mContent.isAdded()) {
+            switchContent(mContent);
+        }
     }
 
+    private Fragment getInitialFragment() {
+        return FilmRollListFragment.newInstance();
+    }
+
+    /**
+     * use for changing currently visible fragment
+     * @param fragment
+     */
     public void switchContent(Fragment fragment) {
+        // store current fragment
+        mContent = fragment;
         // switch content with history
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).addToBackStack(fragment.getClass().getSimpleName()).commit();
         // false by default
         setHomeAsUp(false);
     }
 
+    /**
+     * use for preferences
+     * @return
+     */
     public SharedPreferences getPrefs() {
         return prefs;
     }
