96236c5dcf01126c25e7c9695a758e528406705e
Rene Saarsoo <nene@triin.net>
2017-07-13 10:17:33 +0300

    Remove commented-out code from TemplateFragment

--- app/src/main/java/be/hcpl/android/filmtag/template/TemplateFragment.java
+++ app/src/main/java/be/hcpl/android/filmtag/template/TemplateFragment.java
@@ -16,23 +16,10 @@ public abstract class TemplateFragment extends Fragment {
 
     /**
      * enables us to handle back pressed actions in fragment
-     * @return
      */
     public boolean onBackPressed() {
         return false;
     }
-
-//    /**
-//     * enables dispatching activity results to fragments
-//     *
-//     * @param requestCode
-//     * @param resultCode
-//     * @param data
-//     */
-//    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-//        return false;
-    // TODO handle permissions & other
-//    }
 
     @Nullable
     @Override
