ffe0266e2a3f4231515cc448d2da5174ea765294
Hans Cappelle <hans@roov.app>
2025-11-10 11:37:44 +0100

    apply setting to limit length of notes preview

--- app/src/main/java/be/hcpl/android/filmtag/ui/tranformer/FrameUiModelTransformer.kt
+++ app/src/main/java/be/hcpl/android/filmtag/ui/tranformer/FrameUiModelTransformer.kt
@@ -1,21 +1,27 @@
 package be.hcpl.android.filmtag.ui.tranformer
 
+import be.hcpl.android.filmtag.domain.SettingsRepository
 import be.hcpl.android.filmtag.model.Frame
 import be.hcpl.android.filmtag.ui.view.FrameUiModel
 
 class FrameUiModelTransformer(
     private val textTransformer: TextTransformer,
+    private val settingsRepository: SettingsRepository,
 ) {
 
-    fun transform(frame: Frame) = FrameUiModel(
-        number = frame.number,
-        frameNumber = textTransformer.formatFrameNumber(frame.number),
-        dateTaken = frame.dateTaken?.let { textTransformer.formatDate(it) } ?: "-",
-        apertureAndShutter = textTransformer.formatApertureAndShutter(
-            frame.aperture,
-            frame.shutter,
-            frame.isLongExposure
-        ),
-        frameNotes = frame.notes ?: "-",
-    )
+    fun transform(frame: Frame): FrameUiModel {
+        val formattedNotes = frame.notes ?: ""
+        val previewLimit = settingsRepository.retrieveSettings().limitNotesPreview.takeIf { it > 0 && it < formattedNotes.length }
+        return FrameUiModel(
+            number = frame.number,
+            frameNumber = textTransformer.formatFrameNumber(frame.number),
+            dateTaken = frame.dateTaken?.let { textTransformer.formatDate(it) } ?: "-",
+            apertureAndShutter = textTransformer.formatApertureAndShutter(
+                frame.aperture,
+                frame.shutter,
+                frame.isLongExposure
+            ),
+            frameNotes = previewLimit?.let { "${formattedNotes.substring(0, previewLimit)}..." } ?: formattedNotes,
+        )
+    }
 }
